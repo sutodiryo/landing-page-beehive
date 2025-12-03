f3e417f129e18a19731f8ee307774c8c35c2d174
sutodiryo <hubungi.yogi@gmail.com>
2025-12-03 11:26:46 +0700

    feat - user schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.methods.verifyPassword = function(password) {
+  return bcrypt.compare(password, this.passwordHash);
+}
+
+module.exports = mongoose.model('User', userSchema);
