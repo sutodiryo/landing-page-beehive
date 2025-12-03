08b6505d463bc29aac2b4d22bcda00a34665fdd0
sutodiryo <hubungi.yogi@gmail.com>
2025-12-03 12:11:11 +0700

    feat - articles model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,6 +5,13 @@ const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   passwordHash: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+}, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
+
+// Virtual relation to ResetToken documents
+userSchema.virtual('resetTokens', {
+  ref: 'ResetToken',
+  localField: '_id',
+  foreignField: 'userId',
 });
 
 userSchema.methods.verifyPassword = function(password) {
